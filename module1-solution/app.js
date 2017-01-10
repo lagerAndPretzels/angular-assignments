4a6a41c90c9dea62b32d7d72f5fd439ae0a7e742
Sean <sean@sean.com>
2017-01-09 21:25:57 -0700

    Final version with extra credit.

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,27 +1,34 @@
 (function(){
   angular.module("app",[])
   .controller("ctrlr", function($scope){
-    $scope.lunchList = "";
-    $scope.checkTooMuch = function (){$scope.lunchStatus = checkIfTooMuch($scope.lunchList)};
+    $scope.lunchList = ""; //Initialize variables
     $scope.lunchStatus = "";
+    $scope.checkLunchListUpdateStatus = function (){$scope.lunchStatus = checkIfTooMuch($scope.lunchList)};
   });
   function checkIfTooMuch(string)
   {
-    var response = "";
-    var splitItems = removeEmptyListItems(string.split(","));
-    if(string.length > 0 && splitItems.length > 0){
-      if(splitItems.length > 3) {
-        response = "Too much!";
-      } else {
-        response = "Enjoy!";
+    var response = "Please enter data first";
+    if(string.length > 0){        //Only operate on a string of non-zero length
+      var splitItems = removeEmptyListItems(string.split(","));
+      if(splitItems.length > 0) { //Only operate on a split result of non-zero length
+        if(splitItems.length > 3){ //More than 3 is too much.
+          response = "Too much!";
+        } else {                   //1 to 3 is just right.
+          response = "Enjoy!";
+        }
       }
-    } else {
-      response = "Please enter data first";
     }
     return response;
   }
-  function removeEmptyListItems(string)
+  function removeEmptyListItems(array)
   {
-    return string;
+    for(var i = 0; i < array.length;){
+      if(array[i].trim().length === 0){
+        array.splice(i, 1); //When trimmed and zero, splice it out & continue where left off
+      } else {
+        i++;                //Increment and continue without removal
+      }
+    }
+    return array;
   }
 })();
