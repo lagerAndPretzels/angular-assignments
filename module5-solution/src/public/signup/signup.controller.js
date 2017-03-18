4c9459f2ee4faf706cf35d5e5e3d196a49b3c2c1
Sean <sean@sean.com>
2017-03-18 11:19:29 -0700

    Adjusted implemention of the number

--- module5-solution/src/public/signup/signup.controller.js
+++ module5-solution/src/public/signup/signup.controller.js
@@ -16,27 +16,42 @@ function SignUpController(_MenuService_, _MyInfoService_) {
 
 
   $ctrl.submit = function () {
-    MenuService.getMenuItems($ctrl.category).then(function (response) {
-      var menu_items = response.menu_items;
-      if(menu_items.length){
-        //that indicates a successful category lookup
-        $ctrl.invalidCategory = false;
-        signUpInfo = {};
-        signUpInfo.firstName = $ctrl.firstName;
-        signUpInfo.lastName = $ctrl.lastName;
-        signUpInfo.email = $ctrl.email;
-        signUpInfo.phone = $ctrl.phone;
-        signUpInfo.category = $ctrl.category;
-        console.log("Setting sign up info: ", signUpInfo);
-        MyInfoService.setMyInfo(signUpInfo);
-        $ctrl.lastSubmitSuccess = true;
+    var category = null;
+    var menu_items = null;
+    var number = $ctrl.category.match(/\d/);
+    if(number){ //If we match a number
+      category = $ctrl.category.substring(0,$ctrl.category.indexOf(number));
+      if(category) {
+        category = category.toUpperCase();
+        MenuService.getMenuItems(category).then(function (response) {
+          menu_items = response.menu_items;
+          if(menu_items.length > 0){
+            var myInfo = {};
+            myInfo = {};
+            myInfo.firstName = $ctrl.firstName;
+            myInfo.lastName = $ctrl.lastName;
+            myInfo.email = $ctrl.email;
+            myInfo.phone = $ctrl.phone;
+            myInfo.category = category;
+            myInfo.categoryNumber = $ctrl.category.substring($ctrl.category.indexOf(number));
+            MyInfoService.setMyInfo(myInfo);
+            $ctrl.invalidCategory = false;
+            $ctrl.lastSubmitSuccess = true;
+          } else {
+            console.log("Menu_items: ", menu_items);
+            $ctrl.invalidCategory = true;
+            $ctrl.lastSubmitSuccess = false;
+          }
+        });
       } else {
-        console.log("invalid category");
         $ctrl.invalidCategory = true;
         $ctrl.lastSubmitSuccess = false;
       }
+    } else {
+      $ctrl.invalidCategory = true;
+      $ctrl.lastSubmitSuccess = false;
+    }
 
-    });
   }
 
 
